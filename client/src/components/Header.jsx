0504dae6ae253b656fd689bb8a44a5f46cba0f63
Ankit Dwij <ankitdwiz010@gmail.com>
2023-02-28 00:04:23 +0530

    basic setup

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import brand_logo from "./../assets/bms_logo.png";
+
+const Header = () => {
+  return <>Header</>;
+};
+
+export default Header;
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Layout from "./../components/Layout";
+
+export default function Home() {
+  return (
+    <>
+      <Layout>
+        <div>Hello world</div>
+      </Layout>
+    </>
+  );
+}
